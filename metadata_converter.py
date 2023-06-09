b7b1d804ca6b6616e115e4f7e7882a39d4b7f57d
nicolas <gruber.nicolas@student.htlwrn.ac.at>
2023-06-09 22:29:14 +0200

    cloudbeat changes

--- metadata_converter.py
+++ metadata_converter.py
@@ -1,7 +1,7 @@
 import os
 from tinytag import TinyTag
 from mutagen import File
-from mutagen.mp3 import MP3, EasyMP3
+from mutagen.mp3 import MP3
 from mutagen.id3 import ID3, TIT2, TPE1, TALB, TCON, TRCK, TDRC, APIC
 
 
@@ -11,23 +11,23 @@ def copy_metadata_and_cover(original_file, converted_file):
         original_metadata = TinyTag.get(original_file)
 
         # Load or create ID3 tag in the converted file
-        mp3_file = EasyMP3(converted_file, ID3=ID3)
+        mp3_file = MP3(converted_file, ID3=ID3)
 
         # Copy metadata
         if original_metadata.title:
-            mp3_file.tags['title'] = original_metadata.title
+            mp3_file.tags.add(TIT2(encoding=3, text=original_metadata.title))
         if original_metadata.artist:
-            mp3_file.tags['artist'] = original_metadata.artist
+            mp3_file.tags.add(TPE1(encoding=3, text=original_metadata.artist))
         if original_metadata.album:
-            mp3_file.tags['album'] = original_metadata.album
+            mp3_file.tags.add(TALB(encoding=3, text=original_metadata.album))
         if original_metadata.genre:
-            mp3_file.tags['genre'] = original_metadata.genre
+            mp3_file.tags.add(TCON(encoding=3, text=original_metadata.genre))
         if original_metadata.track:
-            mp3_file.tags['tracknumber'] = str(original_metadata.track)
+            mp3_file.tags.add(TRCK(encoding=3, text=str(original_metadata.track)))
         if original_metadata.year:
-            mp3_file.tags['date'] = str(original_metadata.year)
+            mp3_file.tags.add(TDRC(encoding=3, text=str(original_metadata.year)))
 
-        mp3_file.save(v2_version=3)
+        mp3_file.save()
 
         # Load both files with mutagen
         original_file_tags = File(original_file)
@@ -62,6 +62,6 @@ def copy_metadata_and_cover_in_folder(input_folder, output_folder):
 
 
 # Replace these paths with your actual paths
-input_folder_path = "/path/to/your/input/folder"
-output_folder_path = "/path/to/your/output/folder"
+input_folder_path = "C:/Users/nicol/Desktop/input"
+output_folder_path = "C:/Users/nicol/Desktop/outpout"
 copy_metadata_and_cover_in_folder(input_folder_path, output_folder_path)
--- mp3_converter.py
+++ mp3_converter.py
@@ -11,7 +11,7 @@ def process_file(file_path, target_path):
         return
 
     # Konvertiere oder kopiere die Datei, abhängig vom Dateityp
-    if file_path.lower().endswith(('.flac', '.wav', '.m4a', '.wma', '.aif', '.aiff')):
+    if file_path.lower().endswith(('.flac', '.wav', '.wma', '.aif', '.aiff')):
         print(f"Konvertiere {file_path}...")
         try:
             audio = AudioSegment.from_file(file_path)
@@ -33,7 +33,7 @@ def scan_folders(input_folder, output_folder):
                 os.makedirs(output_path, exist_ok=True)
 
                 # Wenn es sich um eine Audiodatei handelt, konvertieren wir sie in .mp3, sonst behalten wir die Originalerweiterung bei
-                if file_path.lower().endswith(('.flac', '.wav', '.m4a', '.wma', '.aif', '.aiff')):
+                if file_path.lower().endswith(('.flac', '.wav', '.wma', '.aif', '.aiff')):
                     target_path = os.path.join(output_path, os.path.splitext(name)[0] + '.mp3')
                 else:
                     target_path = os.path.join(output_path, name)
@@ -41,7 +41,8 @@ def scan_folders(input_folder, output_folder):
 
 
 # Startpunkt ist der Pfad zu Ihrem Ordner
-# input_folder_path = "C:/Users/nicol/OneDrive/Playlist"
-input_folder_path = "C:/Users/nicol/Desktop/outpout"
-output_folder_path = "C:/Users/nicol/OneDrive/playlist_mp3_iphonefriendly"
+input_folder_path = "C:/Users/nicol/Desktop/input"
+output_folder_path = "C:/Users/nicol/Desktop/outpout"
+# input_folder_path = "C:/Users/nicol/Desktop/outpout"
+# output_folder_path = "C:/Users/nicol/OneDrive/playlist_mp3_iphonefriendly"
 scan_folders(input_folder_path, output_folder_path)
